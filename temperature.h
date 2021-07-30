d8cfc5752617a0671aff54070b4aece9f09ef8a8
Jørund Martinsen <joerundmartinsen@hotmail.com>
2021-07-30 17:02:16 +0200

    added temperature comment

--- temperature.h
+++ temperature.h
@@ -1,5 +1,8 @@
 #include "addresses.h"
 
+// ~25°C -> 22
+
+
 int temperatureArray[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 byte temperatureArrayIdx = 0;
 byte temperatureArrayLength = 20;
